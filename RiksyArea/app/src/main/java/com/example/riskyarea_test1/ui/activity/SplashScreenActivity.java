c87c1a2682a7506a00f1b399d94babf94849692a
Mahadi Hasan Joy <joy35-1207@diu.edu.bd>
2019-10-09 18:01:23 +0600

    LAST UPDATE SEARCH BAR ADDED

--- RiksyArea/app/src/main/java/com/example/riskyarea_test1/ui/activity/SplashScreenActivity.java
+++ RiksyArea/app/src/main/java/com/example/riskyarea_test1/ui/activity/SplashScreenActivity.java
@@ -19,8 +19,8 @@ public class SplashScreenActivity extends AwesomeSplash {
 			/* you don't have to override every property */
 
         //Customize Circular Reveal
-        configSplash.setBackgroundColor(R.color.colorPrimary); //any color you want form colors.xml
-        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setBackgroundColor(R.color.colorPrimaryDark); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(1000); //int ms
         configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
         configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
 
@@ -28,26 +28,26 @@ public class SplashScreenActivity extends AwesomeSplash {
 
         //Customize Logo
         configSplash.setLogoSplash(R.drawable.app_icon); //or any other drawable
-        configSplash.setAnimLogoSplashDuration(2000); //int ms
-        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+        configSplash.setAnimLogoSplashDuration(1000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.Flash); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
 
 
         //Customize Path
        // configSplash.setPathSplash(SyncStateContract.Constants.DROID_LOGO); //set path String
         configSplash.setOriginalHeight(400); //in relation to your svg (path) resource
         configSplash.setOriginalWidth(400); //in relation to your svg (path) resource
-        configSplash.setAnimPathStrokeDrawingDuration(3000);
+        configSplash.setAnimPathStrokeDrawingDuration(1500);
         configSplash.setPathSplashStrokeSize(3); //I advise value be <5
         configSplash.setPathSplashStrokeColor(R.color.colorAccent); //any color you want form colors.xml
-        configSplash.setAnimPathFillingDuration(3000);
-        configSplash.setPathSplashFillColor(R.color.Wheat); //path object filling color
+        configSplash.setAnimPathFillingDuration(1500);
+        configSplash.setPathSplashFillColor(R.color.colorPrimary); //path object filling color
 
 
         //Customize Title
         configSplash.setTitleSplash("Risky Area"); //change your app name here
         configSplash.setTitleTextColor(R.color.colorAccent);
         configSplash.setTitleTextSize(30f); //float value
-        configSplash.setAnimTitleDuration(3000);
+        configSplash.setAnimTitleDuration(1500);
         configSplash.setAnimTitleTechnique(Techniques.FlipInX);
         configSplash.setTitleFont("fonts/Pacifico.ttf"); //provide string to your font located in assets/fonts/
 
