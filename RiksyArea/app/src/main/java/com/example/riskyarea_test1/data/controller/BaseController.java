ca9e0e38df7a82b62fbeaf1cc2b1c1c2cc6e5da0
Raman Karmakar <raman.karmakar@mobioapp.com>
2020-04-19 22:57:07 +0600

    1. Network security config changed 2. Added OKHTTP logger

--- RiksyArea/app/src/main/java/com/example/riskyarea_test1/data/controller/BaseController.java
+++ RiksyArea/app/src/main/java/com/example/riskyarea_test1/data/controller/BaseController.java
@@ -1,5 +1,9 @@
 package com.example.riskyarea_test1.data.controller;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -15,9 +19,20 @@ public class BaseController {
 //                addConverterFactory(GsonConverterFactory.create());
 //        return new Retrofit.Builder().baseUrl("https://riskyarea1.herokuapp.com/").
 //                addConverterFactory(GsonConverterFactory.create());
-        return new Retrofit.Builder().baseUrl("http://206.189.197.151:8888/api/").
+        return new Retrofit.Builder().client(createHttpClient()).baseUrl("http://206.189.197.151:8888/api/").
                 addConverterFactory(GsonConverterFactory.create());
+    }
 
+    private OkHttpClient createHttpClient() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS);
+
+        return builder.build();
     }
 
 }
