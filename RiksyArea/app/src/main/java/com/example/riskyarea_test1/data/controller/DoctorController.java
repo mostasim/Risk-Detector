24e8b7fb3fbc17a38d48cd9b373f1a01abf8b615
Mostasim Billah <mostasim.10@gmail.com>
2020-04-19 15:48:06 +0600

    doctor controller added

---
+++ RiksyArea/app/src/main/java/com/example/riskyarea_test1/data/controller/DoctorController.java
@@ -0,0 +1,37 @@
+package com.example.riskyarea_test1.data.controller;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.riskyarea_test1.data.model.response.Doctor;
+import com.example.riskyarea_test1.data.model.response.Passport;
+import com.example.riskyarea_test1.utils.APIConfig;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class DoctorController extends BaseController {
+    private Retrofit retrofit = getBuilder().build();
+    private APIConfig apiConfig = retrofit.create(APIConfig.class);
+    MutableLiveData<ArrayList<Doctor>> _list = new MutableLiveData<>();
+
+    public LiveData<ArrayList<Doctor>> getPassportList() {
+//        MutableLiveData<ArrayList<PassportListResponse>> _list = new MutableLiveData<>();
+        apiConfig.getDoctorList().enqueue(new Callback<ArrayList<Doctor>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Doctor>> call, Response<ArrayList<Doctor>> response) {
+                _list.postValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Doctor>> call, Throwable t) {
+
+            }
+        });
+        return _list;
+    }
+}
