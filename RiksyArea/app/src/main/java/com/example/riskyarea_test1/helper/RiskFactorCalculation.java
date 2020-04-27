b6e7f6c20375ba94924b1e9f0ad5b089f718adaf
Mostasim Billah <mostasim.10@gmail.com>
2020-04-27 20:47:45 +0600

    1.Question all layout added 2. Question added

--- RiksyArea/app/src/main/java/com/example/riskyarea_test1/helper/RiskFactorCalculation.java
+++ RiksyArea/app/src/main/java/com/example/riskyarea_test1/helper/RiskFactorCalculation.java
@@ -13,14 +13,14 @@ public class RiskFactorCalculation {
 
     public void loadQuestion() {
         questionSets = new ArrayList<GenericQuestion>();
-        GenericQuestion<Integer> age = new GenericQuestion<>(1, "What is your age?", 56);
-        GenericQuestion<Boolean> q_2 = new GenericQuestion<Boolean>(2, "What is your age?", true);
-        GenericQuestion<Boolean> q_3 = new GenericQuestion<Boolean>(3, "What is your age?", true);
-        GenericQuestion<Boolean> q_4 = new GenericQuestion<Boolean>(4, "What is your age?", true);
-        GenericQuestion<Boolean> q_5 = new GenericQuestion<Boolean>(5, "What is your age?", true);
-        GenericQuestion<Boolean> q_6 = new GenericQuestion<Boolean>(6, "What is your age?", true);
-        GenericQuestion<Boolean> q_7 = new GenericQuestion<Boolean>(7, "What is your age?", true);
-        GenericQuestion<Boolean> q_8 = new GenericQuestion<Boolean>(8, "What is your age?", true);
+        GenericQuestion<Integer> age = new GenericQuestion<>(1, "অনুগ্রহ করে আপনার বয়স উল্লেখ করুন", 56);
+        GenericQuestion<Boolean> q_2 = new GenericQuestion<Boolean>(2, "আপনার কি জ্বর আছে বা জ্বরজ্বর অনুভব করছেন ?\nঅর্থাৎ, শরীরের তাপমাত্রা ৩৭.৫°C অথবা, ৯৮.৪°F থেকে বেশি ?", true);
+        GenericQuestion<Boolean> q_3 = new GenericQuestion<Boolean>(3, "আপনার কি কাশি বা গলাব্যথা বা দুইটাই আছে ?", true);
+        GenericQuestion<Boolean> q_4 = new GenericQuestion<Boolean>(4, "আপনার কি শ্বাসকষ্ট আছে বা শ্বাস নিতে বা ফেলতে কষ্ট হচ্ছে ?", true);
+        GenericQuestion<Boolean> q_5 = new GenericQuestion<Boolean>(5, "আপনি কি বিগত ১৪ দিনের ভিতরে বিদেশ হতে এসেছেন?", true);
+        GenericQuestion<Boolean> q_6 = new GenericQuestion<Boolean>(6, "আপনি কি বিগত ১৪ দিনের ভিতরে করোনা ভাইরাসে ( কোবিড-১৯) আক্রান্ত এরকম কোন ব্যক্তির সংস্পর্শে এসেছিলেন ( একই স্থানে অবস্থান বা ভ্রমন ) ?", true);
+        GenericQuestion<Boolean> q_7 = new GenericQuestion<Boolean>(7, "বিগত ১৪ দিনে -জর ,কাশি ,শ্বাসকষ্ট আছে - এমন কারো র সংস্পর্শে কি আপনি এসেছিলেন ( পরিবার সদস্য / অফিস সহকর্মী ) ?", true);
+        GenericQuestion<Boolean> q_8 = new GenericQuestion<Boolean>(8, "   আপনি কি নিম্নের কোন অসুখে ভুগছেন? যেমন : ডায়াবেটিস, উচ্চ রক্তচাপ, হার্টের অসুখ এজমা বা হাঁপানি , দীর্ঘমেয়াদি শ্বাসকষ্টের রোগ বা সিওপিডি, কিডনি রোগ,লিভার রোগ, এইচআইভি, ক্যান্সার বা ক্যান্সারের জন্য কোন চিকিৎসা নিচ্ছেন?", true);
         questionSets.add(age);
         questionSets.add(q_2);
         questionSets.add(q_3);
